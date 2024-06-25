3880ec18483c096da832ac349a404ab775b8bb2e
kaushal16124 <kaushalkishor0000@gmail.com>
2024-06-25 14:30:02 +0530

    Created frontend and backend folders to simplify the file structure for deployment and added the dotenv file

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb://localhost:27017/inotebook";
+require('dotenv').config()
+const mongoURI = "mongodb+srv://testing:Mongo%401234@cluster0.kbkyaa8.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0";
 
 async function connectToMongo() {
-    await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+    await mongoose.connect(process.env.mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
   }
 
   module.exports = connectToMongo;
